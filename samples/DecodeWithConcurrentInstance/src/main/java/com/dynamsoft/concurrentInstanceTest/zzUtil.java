565646040afb03da11819e94d7a8279d87d60242
DMGithubPublisher <github@dynamsoft.com>
2023-08-29 16:03:15 +0800

    update to v9.6.30

--- samples/DecodeWithConcurrentInstance/src/main/java/com/dynamsoft/concurrentInstanceTest/zzUtil.java
+++
@@ -1,8 +0,0 @@
-package com.dynamsoft.concurrentInstanceTest;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-public class zzUtil {
-
-    public static final ObjectMapper jsonMapper = new ObjectMapper();
-}
